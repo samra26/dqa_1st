10b5ceacedcb8cd66ec8b11b97c6876514ecd08e
samra26 <75056386+samra26@users.noreply.github.com>
2023-03-16 04:49:42 +0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -66,8 +66,8 @@ class Solver(object):
         buffer_size = 0
         for buffer in model.buffers():
             buffer_size += buffer.nelement() * buffer.element_size()
-        print(name)
-        print(model)
+        #print(name)
+        #print(model)
         size_all_mb = (param_size + buffer_size) / 1024**2
         print('model size: {:.3f}MB'.format(size_all_mb))
         print("The number of trainable parameters: {}".format(num_params_t))
