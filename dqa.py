9cdacfffdc36851acb3f2a8aee99ae71d90e1ffb
samra26 <75056386+samra26@users.noreply.github.com>
2023-04-02 03:18:20 +0500

    Update dqa.py

--- dqa.py
+++ dqa.py
@@ -1,1 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+import os
+import cv2
+import numpy
+import numpy as np
 
+  
+class DQA(nn.Module):
+    def __init__(self):
+        super(DQA, self).__init__()
+        
+ 
+        
+    def forward(self, rgb,depth):
+        
+        return 
+      
+      
+def build_model():
+ 
+        return DQA()
