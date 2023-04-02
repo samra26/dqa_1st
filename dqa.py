6babb7b67cf1e70d0bac4c313389ef6b0ccb8b52
samra26 <75056386+samra26@users.noreply.github.com>
2023-04-02 11:40:54 +0500

    Update dqa.py

--- dqa.py
+++ dqa.py
@@ -9,14 +9,26 @@ import numpy as np
 
   
 class DQA(nn.Module):
-    def __init__(self):
+    def __init__(self,img_size=384, patch_size=4, in_chans=3,  embed_dim=144):
         super(DQA, self).__init__()
+        self.patches = nn.Sequential(
+            nn.Conv2d(in_chans, embed_dim, 7, 4, 2),
+            Rearrange('b c h w -> b (h w) c', h = img_size//4, w = img_size//4),
+            nn.LayerNorm(embed_dim)
+        )
         
  
         
     def forward(self, rgb,depth):
-        
-        return 
+        B = rgb.shape[0]
+        rgb = self.patches(rgb)
+        B,L,C = rgb.shape
+        B1 = depth.shape[0]
+        depth = self.patches(depth)
+        B1,L1,C1 = depth.shape
+        print('rgb shape patches',rgb.shape)
+        print('depth shape patches',depth.shape)
+        return rgb,depth
       
       
 def build_model():
